5fd9a89e64a4fbd30cb8fc6a0609ae33febdf25d
xuejialiu <xjlelf@163.com>
2014-11-03 10:33:43 +0800

    基本设置

--- trunk/app/Config/core.php
+++ trunk/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhsfasffsfafsEaf63WdhsdhgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '7874561840516748456410');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
@@ -267,7 +267,7 @@
  * Uncomment this line and correct your server timezone to fix
  * any date & time related errors.
  */
-	//date_default_timezone_set('UTC');
+	date_default_timezone_set('PRC');
 
 /**
  * `Config.timezone` is available in which you can set users' timezone string.
